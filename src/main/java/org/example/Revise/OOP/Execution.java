b584a73d02d604aa089da2d013379df629d64e2c
Grzegorz Golonka <golon338@gmail.com>
2023-10-25 22:51:05 +0200

    Firstone

--- src/main/java/org/example/Revise/OOP/Execution.java
+++ src/main/java/org/example/Revise/OOP/Execution.java
@@ -5,5 +5,6 @@ public class Execution {
     public static void main(String[] args) {
         Inherit inherit = new Inherit("First", 12);
         System.out.println(inherit.toString());
+        string xd= "xd";
     }
 }
