eab15661864c94f97c812dd7826a8a4743f9d45c
Grzegorz Golonka <golon338@gmail.com>
2023-10-27 16:36:29 +0200

    revise of inherit record interface and more

---
+++ src/main/java/org/example/Revise/OOP/Combo.java
@@ -0,0 +1,282 @@
+package org.example.Revise.OOP;
+
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+/*
+Dziedziczenie
+Klasy nadrzędne i pochodne
+pochodna dziediczy po klasie nadrzednej
+key word - extends - klasa dziedziczy od innej
+
+ */
+public class Combo {
+
+}
+
+class Figure{
+    private double square;
+
+    public Figure(double square) {
+        this.square = square;
+    }
+    double countSquare(int numberOfSides){
+        return this.square * numberOfSides;
+    }
+
+    public double getSquare() {
+        return square;
+    }
+
+    public void setSquare(double square) {
+        this.square = square;
+    }
+}
+
+class SquareFigure extends Figure{
+    private String nameOfFigure;
+    public SquareFigure(double side, String name){
+        super(side);
+        this.nameOfFigure = name;
+    }
+
+    public String getNameOfFigure() {
+        return nameOfFigure;
+    }
+
+    public void setNameOfFigure(String nameOfFigure) {
+        this.nameOfFigure = nameOfFigure;
+    }
+}
+// -------------------------------------------------------
+/*
+Polimorfizm
+zdolnośc obiektu do przyjmowania wielu form
+   implementacja teg osamego interfejsu
+   przesłanianie metod
+   wielokrotne dziedziczenie interfaców
+ */
+
+class Shape { // base class
+    public void draw(){
+        System.out.println("I draw a figure");
+    }
+}
+
+class Circle extends  Shape { // pochodna klasy
+
+    @Override
+    public void draw() {
+        System.out.println("I draw a cricle");
+    }
+}
+class Rectangle extends  Shape{
+    @Override
+    public void draw() {
+        System.out.println("i draw an rectangle");
+    }
+}
+
+//----------------------------------------------------
+/*
+Abstract classes/ methods
+definiowanie struktur klas i interfasów, które nie maja pełnej implemetacji
+stanowia szkic
+key word abstract
+abstrakcyjne metody musza byc zaimplementowane w klasach pochodnych dziedzicaczych po abstract
+ */
+abstract class Animal{
+    private String name;
+    // abstract method
+    public abstract void makeSound();
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+class Dog extends Animal{
+    public Dog(String name) {
+        super(name);
+    }
+    @Override
+    public void makeSound() {
+        System.out.println("Class:" + getClass().getName() + " with attribute: "  + getName() + " says Woof!");
+    }
+
+}
+
+class Cat extends Animal {
+    public Cat(String name) {
+        super(name);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println(getName() + " says Meow!");
+    }
+}
+/*
+Interasy - mozna dziedziczyć wiele
+*/
+
+interface Vehicle{
+    final int constant_var = 12;
+    void start();
+    void stop();
+}
+interface Radio{
+    void playMusic();
+}
+
+class BMW implements Vehicle, Radio{
+    @Override
+    public void start(){
+        System.out.println("BMW is starting");
+    }
+    @Override
+    public void stop(){
+        System.out.println("BMW stops driving");
+    }
+
+    @Override
+    public void playMusic() {
+        System.out.println("Music is playing" + constant_var + " times");
+    }
+}
+
+/*
+Rekordy: do przechowywania danych , automatycznie generuja
+metody equals hascode to string ettery
+Pomagaja w tworzeniu klas reprezentujacych dane które nie zawieraja dodatkowje logiki
+pola rekordu w {} pola sa niemutowalne - niezmienialne po zainicjowaniu
+nie moga byc rozszerzalne przez dziedziczenie
+*/
+record Book(String title, String author,  int publicationYear){
+    public boolean isBestSeller(){
+        return publicationYear >= 2000;
+    }
+}
+
+class Library{
+    private List<Book> books = new ArrayList<>();
+    public void addBook(Book book){
+        books.add(book);
+    }
+    public List<Book> getBooksPublishedAfterYear(int year){
+        return books.stream()
+                .filter(b -> b.publicationYear() > year)
+                .collect(Collectors.toList());
+    }
+
+}
+/*
+Generics
+ */
+
+class Box<T>{
+    private T content;
+
+    public Box(T content) {
+        this.content = content;
+    }
+    public T getCntent(){
+        return content;
+    }
+}
+class StringBox extends Box<String>{
+    public StringBox(String strContent){
+        super(strContent);
+    }
+}
+//#--- inny przykłąd
+class ListNode<T>{
+    private T data;
+    private ListNode<T> next;
+
+    public ListNode(T data) {
+        this.data = data;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public ListNode<T> getNext() {
+        return next;
+    }
+
+    public void setNext(ListNode<T> next) {
+        this.next = next;
+    }
+}
+
+class GenericList<T>{
+    private ListNode<T> head;
+    public void add (T newData){
+        ListNode<T> newNode = new ListNode<>(newData);
+        if (head == null){
+            head = newNode;
+        }
+        else{
+            ListNode<T> current = head;
+            while (current.getNext() != null){
+
+            }
+        }
+    }
+}
+
+/*
+Serializacja
+-- zapisywanie obiektó do strumienia bajtów
+umożliwia przesyłanie obiektó przez siec i zapisywanie do plików
+Za pomoca interfasu Serializable
+
+ */
+class Person implements Serializable{
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    @Override
+    public String toString() {
+        return "Person [name=" + name + ", age=" + age + "]";
+    }
+
+}
+class Student extends Person{
+    private String id;
+    public Student(String name, int age, String studentId) {
+        super(name, age);
+        this.id = studentId;
+    }
+
+    @Override
+    public String toString() {
+        return "Student [studentId=" + this.id + ", name="  + ", age=" + getAge() + "]";
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/org/example/Revise/OOP/Execution.java
+++ src/main/java/org/example/Revise/OOP/Execution.java
@@ -1,14 +1,92 @@
 package org.example.Revise.OOP;
 
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Execution {
 
     public static void main(String[] args) {
-        Inherit inherit = new Inherit("First", 12);
-        System.out.println(inherit.toString());
-        String xd= "xd";
+        getResults();
+    }
 
-        for (int i =0 ; i <10; i++){
-            System.out.println(xd);
+    static void getResults() {
+        System.out.println("Differences");
+        Figure figure = new Figure(12.5);
+        SquareFigure square = new SquareFigure(12.5, "kwadrat");
+        List<Figure> figures = Arrays.asList(figure, square);
+        List<Figure> figuresSecondWay = new ArrayList<>(List.of(figure, square));
+        for (var fig : figures) {
+            System.out.println(fig.getSquare());
         }
+        cleanOutput();
+//        #-----------------------------
+        /// polimorfizm
+        Shape[] shapes = new Shape[3];
+        shapes[0] = new Circle();
+        shapes[1] = new Rectangle();
+        shapes[2] = new Shape();
+        for (Shape shape : shapes) {
+            shape.draw();
+        }
+//        #------------------
+        //abstract
+        cleanOutput();
+        Animal dog = new Dog("Dragus");
+        Animal cat = new Cat("Mruczek");
+        dog.makeSound();
+        cat.makeSound();
+//        #---------------------------------------
+        // Interfaces
+        cleanOutput();
+        BMW bmw = new BMW();
+        bmw.start();
+        bmw.stop();
+        bmw.playMusic();
+
+        // Rekordy
+        cleanOutput();
+        Library library = new Library();
+
+        Book book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 2001);
+        Book book2 = new Book("To Kill a Mockingbird", "Harper Lee", 1960);
+
+        library.addBook(book1);
+        library.addBook(book2);
+
+        List<Book> recentBooks = library.getBooksPublishedAfterYear(2000);
+
+        System.out.println("Recent Books:");
+        for (Book book : recentBooks) {
+            System.out.println(book);
+        }
+//        #----------------------------
+        cleanOutput();
+        // serializacja
+        try{
+            ObjectOutputStream output = new ObjectOutputStream(
+                    new FileOutputStream("student.ser")
+            );
+            Student student = new Student("Alice", 20, "1234");
+            output.writeObject(student);
+            System.out.println(student);
+        }
+        catch (Exception exception){
+            exception.printStackTrace();
+        }
+        // Deserializacja
+        try (ObjectInputStream inputStream = new ObjectInputStream(
+                new FileInputStream("student.ser"))) {
+            Student deserializedStudent = (Student) inputStream.readObject();
+            System.out.println("Deserialized: " + deserializedStudent);
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void cleanOutput() {
+        System.out.println("\n-----------------------------------\n");
     }
 }
